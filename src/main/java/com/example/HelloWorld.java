16ad15e86c7be60225b69449bd699975f02ca4aa
CapFib <108801686+CapFib@users.noreply.github.com>
2026-04-15 16:48:53 +0800

    New function

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -3,7 +3,7 @@ package com.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); //comment 1
-        System.out.println(getNew());
+        System.out.println("Automation is Wow");
     }
 
     public String getGreeting() {
